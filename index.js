ebb0e75fbded208aee4036d896645dcc64e5c094
brian corker <bcorker123@gmail.com>
2022-05-02 11:08:41 -0400

    commit tree c04acf3352befaa7d31e6a726e11a2a762303056

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.querySelector('#dodger');
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+})
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', e => {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+})
